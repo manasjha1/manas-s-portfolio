ef036595d930a03178fcd3623b11d3271b32e48d
manas jha <manasjha384@gmail.com>
2026-07-26 17:50:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ function App() {
       <Header />
 
       {/* 4. Portfolio Sections Container */}
-      <main className="w-full flex flex-col relative z-10 overflow-hidden">
+      <main className="w-full flex flex-col relative z-10 overflow-hidden mx-auto">
         <Hero />
         <Intro />
         <Ticker />
